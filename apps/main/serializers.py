f148ec4778227b766f9a1a6b3205ea04f59c703e
goutom <goutom.sust.cse@gmail.com>
2020-01-04 21:06:43 +0600

    Improved.

--- apps/main/serializers.py
+++ apps/main/serializers.py
@@ -32,57 +32,11 @@ class OrderItemSerializer(serializers.ModelSerializer):
         return value
 
 
-# class OrderItemSerializerNested(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = OrderItem
-#         fields = ('id', 'order', 'pizza', 'size', 'quantity')
-#         # read_only_fields = ('order',)
-#         # validators = [UniqueTogetherValidator(
-#         #     queryset=OrderItem.objects.all(),
-#         #     fields=['order', 'pizza', 'size'],
-#         #     message="duplicate key value violates unique constraint 'Unique order pizza size'")
-#         # ]
-#         # extra_kwargs = {'order': {'required': False}}
-#
-#     # def get_extra_kwargs(self):
-#     #     action = self.context['view'].action
-#     #     if action in ['create']:
-#     #         extra_kwargs = {'order': {'required': False}}
-#     #         return extra_kwargs
-#     #     elif action in ['update', 'partial_update']:
-#     #         extra_kwargs = {'id': {'read_only': False, 'required': True},
-#     #                         'order': {'required': False}}
-#     #         return extra_kwargs
-#     #     else:
-#     #         return {}
-
 class OrderItemSerializerNested(serializers.ModelSerializer):
-
-    # id = IntegerField(label='ID', required=False)
-    # order = PrimaryKeyRelatedField(queryset=Order.objects.all(), required=False)
-    # pizza = PrimaryKeyRelatedField(queryset=Pizza.objects.all())
-    # size = ChoiceField(choices=[(1, 'SMALL'), (2, 'MEDIUM'), (3, 'BIG')],
-    #                    validators=[MinValueValidator(1), MaxValueValidator(3)])
-    # quantity = IntegerField(max_value=1000, min_value=1)
 
     class Meta:
         model = OrderItem
         fields = ('id', 'order', 'pizza', 'size', 'quantity')
-
-    def get_extra_kwargs(self):
-        action = self.context['view'].action
-        if action in ['create']:
-            extra_kwargs = {'id': {'read_only': False, 'required': False},
-                            'order': {'required': False}}
-            return extra_kwargs
-        elif action in ['update', 'partial_update']:
-            print('------get_extra_kwargs----------')
-            extra_kwargs = {'id': {'read_only': False, 'required': True},
-                            'order': {'required': False}}
-            return extra_kwargs
-        else:
-            return super().get_extra_kwargs()
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -98,45 +52,4 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ('id', 'user', 'status', 'created', 'order_items')
         read_only_fields = ('user', )
 
-    # def get_extra_kwargs(self):
-    #     action = self.context['view'].action
-    #     if action in ['create', 'update', 'partial_update']:
-    #         extra_kwargs = {'order_items': {'required': False}}
-    #         return extra_kwargs
-    #     else:
-    #         return {}
-
-    def create(self, validated_data):
-        order_items = validated_data.pop('order_items')
-        order = Order.objects.create(user=self.context['request'].user)
-        for oi in order_items:
-            OrderItem.objects.create(order=order, **oi)
-        return order
-
-    def update(self, instance, validated_data):
-        action = self.context['view'].action
-
-        print(f"method : partial_update {validated_data}")
-        order_items = validated_data.pop('order_items')
-        status = validated_data.get('status', None)
-        if status:
-            instance.status = status
-            instance.save()
-
-    def validate_status(self, value):
-        """
-        Status validation can be changed based on business rules.
-        """
-
-        action = self.context['view'].action
-
-        if action in ['update', 'partial_update']:
-
-            if value == StatusTypes.CANCELED and self.instance.status >= StatusTypes.IN_PRODUCTION:
-                raise serializers.ValidationError("Status can't be changed now, because its in process of delivery.")
-
-            elif value < self.instance.status:
-                raise serializers.ValidationError("Status can't be downgraded.")
-
-        return value
 
